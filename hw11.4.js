97dbaf241e257c9a53be175129e0f8a21e5b20ad
Jeļena <git.narrenturm@gmail.com>
2020-11-27 12:32:55 +0200

    убрана избыточность (в функции IsInteger() нет смысла)

--- hw11.4.js
+++ hw11.4.js
@@ -2,9 +2,8 @@
 Напишите функцию, которая принимает два числа. Каждую секунду необходимо выводить в консоль, начиная от первого и заканчивая вторым. Используйте setInterval.
 */
 
-let IsInteger = a => Number.isInteger(a) ? true : false;
 let BlinkNumber = (a, b) => {
-  if(!IsInteger(a) || !IsInteger(b)) return `введены неправильные параметры`;
+  if(!Number.isInteger(a) || !Number.isInteger(b)) return `введены неправильные параметры`;
   let interval = setInterval(function() {
     console.log(a);
     if(a == b) clearInterval(interval);
